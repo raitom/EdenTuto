f0b2c6b4265d5ac60df079e7e85cbf8c012ad583
Thomas Geselle <thomas@Thomas-Mac-mini.local>
2013-05-08 12:14:06 +0200

    Rails 3.2 tout frais !

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdenTuto::Application.config.secret_token = '34c5a6ef34697f5b216b26395e1ec0b597ab20b61783b815a3bf73f4b8b785a7872cf6abe360c077dccc086b17b70a1e3ec70ade094409b1b7b09777e070dde4'
